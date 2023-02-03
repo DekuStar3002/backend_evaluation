a1f79f890aec1f915ed56dbb7f3fe8ea6ab91965
Rahul Behera <rahulbehera2002@gmail.com>
2023-02-03 12:10:13 +0530

    feat: add company model and sector model to database

---
+++ database/migrations/20230203062937-create-company.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Companies', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      ceo: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      score: {
+        type: Sequelize.INTEGER
+      },
+      sector_id: {
+        type: Sequelize.INTEGER
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Companies');
+  }
+};
