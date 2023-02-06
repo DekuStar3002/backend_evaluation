f2f057705448f492008b74e20e97483d98c47475
Rahul Behera <rahulbehera2002@gmail.com>
2023-02-06 12:31:22 +0530

    test: add testcase for queryValidation.idValidaton middleware

--- test/middleware/queryValidation.test.js
+++ test/middleware/queryValidation.test.js
@@ -31,4 +31,35 @@ describe('Query Validation Middleware', () => {
     expect(mockRes.status).toBeCalledWith(400);
     expect(mockRes.json).toBeCalledWith({ message: "\"sector\" is required" });
   });
+  
+  it('should call next method when queryValidation.idValidaton called', async () => {
+    const mockReq = {
+      query: {
+        id: '1234'
+      }
+    };
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    const mockNext = jest.fn();
+    queryValidaton.idValidaton(mockReq, mockRes, mockNext);
+    expect(mockNext).toBeCalled();
+  });
+
+  it('should throw error when queryValidation.sectorValidaton cause error', async () => {
+    const mockReq = {
+      query: {
+        id: 1234
+      }
+    };
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    const mockNext = jest.fn();
+    queryValidaton.idValidaton(mockReq, mockRes, mockNext);
+    expect(mockRes.status).toBeCalledWith(400);
+    expect(mockRes.json).toBeCalledWith({ message: "\"id\" must be a string" });
+  });
 });
